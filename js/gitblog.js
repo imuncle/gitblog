1f226ebacfb5a7c4e050a93c565fcf0032eb779a
imuncle <big.uncle@foxmail.com>
2019-07-15 12:46:18 +0800

    修改时间显示格式

--- js/gitblog.js
+++ js/gitblog.js
@@ -51,7 +51,16 @@ var gitblog = function(config) {
     self.utc2localTime = function(time) {
         var time_string_utc_epoch = Date.parse(time);
         var unixTimestamp = new Date(time_string_utc_epoch);
-        return unixTimestamp.toLocaleString();
+        var year = unixTimestamp.getFullYear();
+        var month = unixTimestamp.getMonth();
+        var date = unixTimestamp.getDate();
+        var hour = unixTimestamp.getHours();
+        var minute = unixTimestamp.getMinutes();
+        var second = unixTimestamp.getSeconds();
+        hour = (hour<10)?'0'+hour:hour;
+        minute = (minute<10)?'0'+minute:minute;
+        second = (second<10)?'0'+second:second;
+        return year+'年'+month+'月'+date+'日'+' '+hour+':'+minute+':'+second;
     }
 
     String.prototype.replaceAll = function(a, b) {
@@ -376,7 +385,7 @@ var gitblog = function(config) {
         show: function(type, id) {
             var reaction = this;
             if (reaction.isLike == false) {
-                document.getElementById(id).innerHTML = '<svg style="height:20px;float:left" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 50 50"><path d="M25 39.7l-.6-.5C11.5 28.7 8 25 8 19c0-5 4-9 9-9 4.1 0 6.4 2.3 8 4.1 1.6-1.8 3.9-4.1 8-4.1 5 0 9 4 9 9 0 6-3.5 9.7-16.4 20.2l-.6.5zM17 12c-3.9 0-7 3.1-7 7 0 5.1 3.2 8.5 15 18.1 11.8-9.6 15-13 15-18.1 0-3.9-3.1-7-7-7-3.5 0-5.4 2.1-6.9 3.8L25 17.1l-1.1-1.3C22.4 14.1 20.5 12 17 12z"></path></svg>';
+                document.getElementById(id).innerHTML = '<img src="images/heart.svg" style="height:20px;float:left">';
             } else if (reaction.isLike == true) {
                 document.getElementById(id).innerHTML = '❤️';
             }
@@ -503,7 +512,7 @@ var gitblog = function(config) {
                     }
                 });
             } else {
-                avatar.innerHTML = '<a class="gitment-editor-avatar" id="gitment-avatar" title="login with GitHub">' + '<svg class="gitment-github-icon" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 50 50"><path d="M25 10c-8.3 0-15 6.7-15 15 0 6.6 4.3 12.2 10.3 14.2.8.1 1-.3 1-.7v-2.6c-4.2.9-5.1-2-5.1-2-.7-1.7-1.7-2.2-1.7-2.2-1.4-.9.1-.9.1-.9 1.5.1 2.3 1.5 2.3 1.5 1.3 2.3 3.5 1.6 4.4 1.2.1-1 .5-1.6 1-2-3.3-.4-6.8-1.7-6.8-7.4 0-1.6.6-3 1.5-4-.2-.4-.7-1.9.1-4 0 0 1.3-.4 4.1 1.5 1.2-.3 2.5-.5 3.8-.5 1.3 0 2.6.2 3.8.5 2.9-1.9 4.1-1.5 4.1-1.5.8 2.1.3 3.6.1 4 1 1 1.5 2.4 1.5 4 0 5.8-3.5 7-6.8 7.4.5.5 1 1.4 1 2.8v4.1c0 .4.3.9 1 .7 6-2 10.2-7.6 10.2-14.2C40 16.7 33.3 10 25 10z"></path></svg>' + '</a></div>';
+                avatar.innerHTML = '<a class="gitment-editor-avatar" id="gitment-avatar" title="login with GitHub">' + '<img src="images/gitment-github-icon.svg" class="gitment-github-icon">' + '</a></div>';
                 document.getElementById('gitment-avatar').onclick = function() {
                     comment.log();
                 }
